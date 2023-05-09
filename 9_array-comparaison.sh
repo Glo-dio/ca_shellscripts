2f6f5752c162f089a62fd9fa5ed5168d61e6dfa3
Glodi PETA <gpeta@e2r5p4.clusters.42paris.fr>
2023-05-09 12:10:37 +0200

    Learn the Basics 9/10 done

--- 9_array-comparaison.sh
+++ 9_array-comparaison.sh
@@ -7,41 +7,27 @@ echo ""
 echo "EXERCICE"
 echo "--------"
 
+# Trouver le(s) chiffre(s) communs entre 3 listes
+
 a=(3 5 8 10 6) b=(6 5 4 12) c=(14 7 5 7)
 echo "a = ${a[@]}"
 echo "b = ${b[@]}"
 echo "c = ${c[@]}"
 echo ""
 
-i=0
-max=${#a[@]}
-
-while [ $i -lt $max ] ; do
-	i=$(($i+1))
-	if 
-	echo bonjour
+for x in ${a[@]}; do
+	for y in ${b[@]}; do
+		if [ $x = $y ]; then
+			z[${z[@]}]=$x
+		fi
+	done
 done
 
-
-#comparison of first two arrays a and b
-	for x in "${a[@]}"; do 
-	  in=false 
-	  for y in "${b[@]}"; do 
-	    if [ $x = $y ];then 
-	      # assigning the matching results to new array z
-	      z[${#z[@]}]=$x
-	    fi
-	  done 
+for i in ${c[@]}; do
+	for j in ${z[@]}; do
+		if [ $i = $j ]; then
+			k[${k[@]}]=$i
+		fi
 	done
-	#comparison of third array c with new array z
-	for i in "${c[@]}"; do 
-	  in=false
-	  for k in "${z[@]}"; do
-	    if [ $i = $k ];then
-	      # assigning the matching results to new array j
-	      j[${#j[@]}]=$i
-	    fi
-	  done 
-	done 
-	# print content of array j
-	echo ${j[@]}
+done
+echo ${k[@]}
